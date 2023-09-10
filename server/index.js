3f002fff8ce865ce3615c4f81a88d751635df804
Ayo <heyayomideadebisi@gmail.com>
2023-09-10 21:42:16 +0100

    feat: init socket server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require("express")
 const app = express()
+const http = require("http")
+const cors = require("cors")
+const { Server } = require("socket.io")
 port = 8080
 
-app.get("/", (req, res) => {
-    res.status(200).json({msg: "Hello"})
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    },
 })
 
-app.listen(port, () => console.log(`server is listening on port: ${port}`))
+server.listen(port, () => console.log(`server is listening on port: ${port}`))
